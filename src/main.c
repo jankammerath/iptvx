ada45121d897ad5d7da782de3937858cfb4f40a0
Jan Kammerath <jankammerath@gmail.com>
2017-01-11 20:29:56 +0100

    Implemented argp

--- src/main.c
+++ src/main.c
@@ -3,15 +3,22 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include "args.c"
 #include "window.c"
 
 /* main application code */
 int main (int argc, char *argv[]){
-	iptvx_create_window_thread();
+	/* parse input arguments first */
+	struct arguments arguments = iptvx_parse_args(argc,argv);
+
+	/* ensure sufficient parameters where provided */
+	if(arguments.sufficient == true){
+		iptvx_create_window_thread();
 	
-	while(1){
-		printf("I am still here!\n");
-		usleep(5000000);
+		while(1){
+			printf("I am still here!\n");
+			usleep(5000000);
+		}
 	}
 
 	return 0;
