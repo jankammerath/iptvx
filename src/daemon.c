cca160385ef2ff7ee3ac8f26781c92c83d8be3d9
Jan Kammerath <jankammerath@gmail.com>
2017-12-26 09:23:26 +0100

    issue #48 daemon can now take new recordings

--- src/daemon.c
+++ src/daemon.c
@@ -22,6 +22,7 @@
 #include <string.h>
 #include <microhttpd.h>
 #include <glib.h>
+#include <json-c/json.h>
 
 /* indicator to stay alive */
 volatile sig_atomic_t iptvx_daemon_alive;
@@ -40,6 +41,26 @@ GString* iptvx_daemon_epg_json;
 
 /* garray with all epg data */
 GArray* iptvx_daemon_epg_data;
+
+/* a list with all recordings */
+GArray* recordlist;
+
+/* represents a single recording */
+struct recording{
+   /* name of the channel to record */
+   GString* channel;
+
+   /* start and stop timings */
+   long start;
+   long stop;
+
+   /* 0 = scheduled, 1 = in progress, 
+      2 = ready, 9 = failed */
+   int status;
+
+   /* filename of the recording */
+   GString* filename;
+} typedef recording;
 
 /*
    Sets the recording tolerance in minutes
@@ -90,11 +111,96 @@ void iptvx_daemon_kill(){
 }
 
 /*
+   Returns the list with all recordings as json
+   @return        list with all recordings as json
+*/
+GString* iptvx_daemon_get_recordlist_json(){
+   GString* result;
+
+   json_object* j_rec_array = json_object_new_array();
+
+   if(recordlist != NULL){
+      int c = 0;
+      for(c = 0; c < recordlist->len; c++){
+         /* fetch the recording from the array */
+         recording* rec = &g_array_index(recordlist,recording,c);
+
+         /* create json object for this recording */
+         json_object* j_rec = json_object_new_object();
+         json_object_object_add(j_rec,"channel",
+               json_object_new_string(rec->channel->str));
+         json_object_object_add(j_rec,"filename",
+               json_object_new_string(rec->filename->str));
+         json_object_object_add(j_rec,"start",
+               json_object_new_int(rec->start));
+         json_object_object_add(j_rec,"stop",
+               json_object_new_int(rec->stop));
+         json_object_object_add(j_rec,"status",
+               json_object_new_int(rec->status));
+
+         /* add to json array */
+         json_object_array_add(j_rec_array,j_rec);
+      }
+   }
+
+   /* finally pass j_object to result string */
+   result = g_string_new(json_object_to_json_string(j_rec_array));
+
+   return result;
+}
+
+/*
+   Creates a new recording struct from given data
+   @param      channel     name of channel to record
+   @param      start       timestamp of start time
+   @param      stop        timestamp of stop time
+   @return                 the recording struct with the data   
+*/
+recording iptvx_daemon_create_recording(char* channel, long start, long stop){
+   recording result;
+
+   result.channel = g_string_new(channel);
+   result.start = start;
+   result.stop = stop;
+   result.status = 0;
+   result.filename = g_string_new("");
+
+   return result;
+}
+
+/*
+   Adds a new recording to the list if it does not already exist
+   @param         rec         the new recording to add to the list
+*/
+void iptvx_daemon_add_recording(recording rec){
+   bool exists = false;
+
+   int c;
+   for(c = 0; c<recordlist->len;c++){
+      recording* exist_rec = &g_array_index(recordlist,recording,c);
+
+      /* check if this is the same or equal */
+      if(g_strcmp0(exist_rec->channel->str,rec.channel->str)==0 
+         && exist_rec->start == rec.start
+         && exist_rec->stop == rec.stop){
+         /* set the marker, we have it already */
+         exists = true;
+      }
+   }
+
+   if(!exists){
+      /* this is a new one, so add it */
+      g_array_append_val(recordlist,rec);
+   }
+}
+
+/*
    Provides the proper result for the url
    @param      request_url    url requested from the client
+   @param      connection     handle for the current connection
    @return                    string with JSON contents to show
 */
-GString* iptvx_daemon_get_response(char* request_url){
+GString* iptvx_daemon_get_response(char* request_url, struct MHD_Connection* connection){
    GString* result = g_string_new("{}");
 
    /* check for requested data */
@@ -103,6 +209,34 @@ GString* iptvx_daemon_get_response(char* request_url){
       if(iptvx_daemon_epg_json != NULL){
          result = iptvx_daemon_epg_json;
       }
+   }if(g_strcmp0(request_url,"/record.json")==0){
+      /* recorder information requested */
+      const char* requestedAction = MHD_lookup_connection_value 
+                     (connection, MHD_GET_ARGUMENT_KIND, "action");
+
+      /* check for any actions requested */
+      if(g_strcmp0(requestedAction,"add")==0){
+         /* requested to add new recording */
+         const char* rec_channel = MHD_lookup_connection_value 
+                  (connection, MHD_GET_ARGUMENT_KIND, "channel");
+         const char* rec_start = MHD_lookup_connection_value 
+                  (connection, MHD_GET_ARGUMENT_KIND, "start");
+         const char* rec_stop = MHD_lookup_connection_value 
+                  (connection, MHD_GET_ARGUMENT_KIND, "stop");
+
+         /* esnure values exist and are not null */
+         if(rec_channel != NULL && rec_start != NULL && rec_stop != NULL){
+            /* create new recording struct */
+            recording new_rec = iptvx_daemon_create_recording((char*)rec_channel,
+                                                atol(rec_start), atol(rec_stop));
+
+            /* append new recording to list */
+            iptvx_daemon_add_recording(new_rec);
+         }
+      }
+
+      /* output list with recordings */
+      result = iptvx_daemon_get_recordlist_json();
    }
 
    return result;
@@ -120,7 +254,7 @@ static int iptvx_daemon_handle_request(void * cls, struct MHD_Connection * conne
   int ret;
 
   /* define the response */
-  GString* content = iptvx_daemon_get_response((char*)url);
+  GString* content = iptvx_daemon_get_response((char*)url,connection);
 
   /* create the response for the query */
   response = MHD_create_response_from_buffer(strlen(content->str), (void*)content->str,
@@ -144,6 +278,9 @@ void iptvx_daemon_run(){
    /* indicate the daemon to stay alive */
    iptvx_daemon_alive = true;
 
+   /* initialise recording list */
+   recordlist = g_array_new(false,false,sizeof(recording));
+
    /* attach sigterm handler to kill daemon */
    struct sigaction action;
    memset(&action, 0, sizeof(struct sigaction));
@@ -153,6 +290,7 @@ void iptvx_daemon_run(){
    /* create the http lib */
    struct MHD_Daemon * d;
 
+   /* start the http daemon and listen */
    d = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION,
              iptvx_daemon_server_port, NULL, NULL,
              &iptvx_daemon_handle_request,
